e46a381117b616499867a6389d152f70a7920f07
Pedro Henrique Santos Cunha <hnrqpdr.sc@gmail.com>
2021-11-28 18:35:19 -0300

    commit tentativa 3d, ainda com erros mas fizemos avanços

---
+++ stormer-verlet_method/sv_3d.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define M 1.6726219e-27
+#define alpha1 3.037e3
+
+//LEMBRAR DE TROCAR O NOME DO ARQUIVO DE SAÍDA!
+
+/*double effectivePotential(double rho, double c20){
+    return (c20*c20)/((2*M*M)*rho*rho) - (alpha1*c20)/(M*rho*rho*rho) + (alpha1*alpha1)/(2*rho*rho*rho*rho);
+}*/
+
+double dHdp(double rho, double z, double c2){
+
+    double R = sqrt(rho*rho + z*z);
+    double expression = (c2*c2/(M*rho*rho*rho)) + 3*((M*alpha1*alpha1*rho*rho*rho)/(R*R*R*R*R*R*R*R)) - ((M*alpha1*alpha1*rho)/(R*R*R*R*R*R)) - (3*alpha1*c2*rho)/(R*R*R*R*R);
+
+    return expression;
+}
+
+double dHdz(double rho, double z, double c2){
+
+    double R = sqrt(rho*rho + z*z);
+    double expression = (3*M*alpha1*alpha1)*((rho*rho*z)/(R*R*R*R*R*R*R*R)) - (3*alpha1*c2)*((z)/(R*R*R*R*R));
+
+    return expression;
+}
+
+/*double dHdp(double rho, double z, double c2){
+
+    double R = sqrt(rho*rho + z*z);
+    double expression = 1/(M*M)*((c2/rho) - (M*alpha1*rho/(R*R*R)))*(-(c2/(rho*rho)) - (M*alpha1/(R*R*R)) + (3.0/2.0)*(M*alpha1/(R*R*R*R*R)));
+
+    return expression;
+}*/
+
+/*double dHdz(double rho, double z, double c2){
+
+    double R = sqrt(rho*rho + z*z);
+    double expression =  ((3*alpha1*rho*z)/(M*R*R*R*R*R))*((c2/rho) - (M*alpha1*rho/(R*R*R)));
+
+    return expression;
+}*/
+
+void polar2cartesian(double *rho, double *drho, double *phi, double dphi, double *x, double *dx, double *y, double *dy, double TAM) {
+    for(int n = 0; n<= TAM; n++){
+
+        x[n] = rho[n]*cos(phi[n]);
+        dx[n] = drho[n]*cos(phi[n]) - rho[n]*sin(phi[n])*dphi;
+        y[n] = rho[n]*sin(phi[n]);
+        dy[n] = drho[n]*sin(phi[n]) + rho[n]*cos(phi[n])*dphi;
+    }
+}
+
+void stormer_verlet_method(double *rho, double *drho, double *phi, double dphi, double *z, double *dz, double *v_eff, double deltaT, double *c2, double TAM) {
+
+    double drho_aux, dz_aux;
+    
+    for(int n = 1; n<=TAM; n++){
+
+        //auxiliares
+        drho_aux = drho[n-1] - deltaT*dHdp(rho[n-1], z[n-1], c2[n-1]);
+        dz_aux = dz[n-1] - deltaT*dHdz(rho[n-1], z[n-1], c2[n-1]);
+        //coordenadas
+        rho[n] = rho[n-1] +  deltaT*drho_aux;
+        z[n] = z[n-1] + deltaT*dz_aux;
+        phi[n] = phi[n-1] + deltaT*((c2[n-1])/(M*rho[n-1]*rho[n-1]) - (alpha1/(sqrt(rho[n-1]*rho[n-1] + z[n-1]*z[n-1])*sqrt(rho[n-1]*rho[n-1] + z[n-1]*z[n-1])*sqrt(rho[n-1]*rho[n-1] + z[n-1]*z[n-1]))));
+        c2[n] = M*rho[n]*rho[n]*dphi + (M*alpha1*rho[n]*rho[n])/(sqrt(rho[n]*rho[n] + z[n]*z[n])*sqrt(rho[n]*rho[n] + z[n]*z[n])*sqrt(rho[n]*rho[n] + z[n]*z[n]));
+
+        //velocidades
+        drho[n] = drho_aux - deltaT*dHdp(rho[n], z[n], c2[n]);
+        dz[n] = dz_aux - deltaT*dHdz(rho[n], z[n], c2[n]);
+
+    }
+}
+
+void getData(char fileParticle[], char filePotential[], double *rho, double *drho, double *phi, double dphi, double *x, double *dx, double *y, double *dy, double *z, double *dz, double *v_eff, double TAM){
+
+    FILE *particle, *effectivePotential;
+
+    particle = fopen(fileParticle, "w");
+    effectivePotential = fopen(filePotential, "w");
+
+    fprintf(particle, "n\tx\ty\tz\n");
+    fprintf(effectivePotential, "n\tv_eff\trho\n");
+
+    polar2cartesian(rho, drho, phi, dphi, x, dx, y, dy, TAM);
+    for(int n = 0; n<=TAM; n++){
+        fprintf(particle, "%d %g %g %g\n", n, x[n], y[n], z[n]);
+    }
+
+    /*for(int n = 0; n<=TAM; n++){
+        fprintf(effectivePotential, "%d %g %g\n", n, v_eff[n], rho[n]);
+    }*/
+
+    fclose(particle);
+    fclose(effectivePotential);
+}
+
+int main(int argc, char *argv[]){
+    int TAM;
+    double *rho, *phi, *drho, dphi, *x, *dx, *y, *dy, *z, *dz, *v_eff, *c2, deltaT = 0.00002, c20;
+
+    TAM = atof(argv[1])/deltaT;
+
+    rho =  malloc((TAM)*sizeof(double));
+    drho =  malloc((TAM)*sizeof(double));
+    phi =  malloc((TAM)*sizeof(double));
+    x =  malloc((TAM)*sizeof(double));
+    dx = malloc((TAM)*sizeof(double));
+    y =  malloc((TAM)*sizeof(double));
+    dy = malloc((TAM)*sizeof(double));
+    z = malloc((TAM)*sizeof(double));
+    dz = malloc((TAM)*sizeof(double));
+    v_eff = malloc((TAM)*sizeof(double));
+    c2 = malloc((TAM)*sizeof(double));
+
+    rho[0] =  atof(argv[2]);
+    drho[0] =  atof(argv[3]);
+    phi[0] =  atof(argv[4]);
+    dphi =  atof(argv[5]); // a derivada de phi é constante.
+    z[0] = atof(argv[6]);
+    dz[0] = atof(argv[7]);
+    c2[0] = M*rho[0]*rho[0]*dphi + (M*alpha1*rho[0]*rho[0])/(sqrt(rho[0]*rho[0] + z[0]*z[0])*sqrt(rho[0]*rho[0] + z[0]*z[0])*sqrt(rho[0]*rho[0] + z[0]*z[0]));
+
+    stormer_verlet_method(rho, drho, phi, dphi, z, dz, v_eff, deltaT, c2, TAM);
+
+    getData(argv[8], argv[9], rho, drho, phi, dphi, x, dx, y, dy, z, dz, v_eff, TAM);
+
+    free(rho);
+    free(drho);
+    free(phi);
+    free(x);
+    free(dx);
+    free(y);
+    free(dy);
+    free(v_eff);
+    free(z);
+    free(dz);
+    free(c2);
+}
