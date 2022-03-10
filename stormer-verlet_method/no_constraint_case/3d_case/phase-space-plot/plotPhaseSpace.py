52d9f3cfd9b9089802a6be88b2808796cd137d4d
Shnrqpdr <hnrqpdr.sc@gmail.com>
2022-03-10 14:19:10 -0300

    plot espaço de fase

--- stormer-verlet_method/no_constraint_case/3d_case/phase-space-plot/plotPhaseSpace.py
+++ stormer-verlet_method/no_constraint_case/3d_case/phase-space-plot/plotPhaseSpace.py
@@ -5,10 +5,10 @@ fig_1 = plt.figure(1)
 
 ax1=fig_1.add_subplot(111)
 
-contador = 80
+contador = 20
 
-while(contador <= 85):
-    fileProton=pd.read_csv(f'phaseSpaceProton{contador}.dat', header = 0, sep='\s+')
+while(contador <= 23):
+    fileProton=pd.read_csv(f'data-PlotPhaseSpace/phaseSpaceProton{contador}.dat', header = 0, sep='\s+')
     label = 'Proton'
     ax1.plot(fileProton['rho'],fileProton['drho'], '-', label=label, linewidth = 0.4)
     contador = contador+1
@@ -18,11 +18,11 @@ while(contador <= 85):
 ax1.set(ylim=(-100, 100)) # se for plotar com 2 protons, retirar essa lin
 plt.grid(linestyle='-', linewidth=0.7)
 plt.xlabel(r'$\rho$')
-plt.ylabel(r'$\dfrac{d\rho}{dt}$')
+plt.ylabel(r'$\dot{\rho}$')
 
 
-plt.title("Mapas de Poincaré associados à constante de movimento", fontdict=None,)
+plt.title("Espaço de Fase", fontdict=None,)
 #plt.legend(loc="upper right",ncol=3,fontsize='small')
 
 plt.show()
-plt.savefig('phaseSpace2.pdf')
+plt.savefig('phaseSpace20-23.pdf', dpi=600)
