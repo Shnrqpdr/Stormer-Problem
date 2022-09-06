039ecfabe95cbd30e36d7502095f3f31b224c5cc
Shnrqpdr <hnrqpdr.sc@gmail.com>
2022-09-06 20:12:28 -0300

    Novas simulações

---
+++ stormer-verlet_method/no_constraint_case/3d_case/scriptRun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./sv3d 2 3.0 5.0 0.0 10.0 0.5 0.0 dadosProton1Animacao.dat phaseSpaceP1_2.dat
+./sv3d 2 3.0 10.0 3.14 10.0 0.5 0.0 dadosProton2Animacao.dat phaseSpaceP2_2.dat
+./sv3d 2 3.0 25.0 5.23 10.0 0.5 0.0 dadosProton3Animacao.dat phaseSpaceP3_2.dat
+./sv3d 2 3.0 50.0 0.0 10.0 0.5 0.0 dadosProton4Animacao.dat phaseSpaceP4_2.dat
+./sv3d 2 3.0 75.0 3.14 10.0 0.5 0.0 dadosProton5Animacao.dat phaseSpaceP5_2.dat
+./sv3d 2 3.0 90.0 5.23 10.0 0.5 0.0 dadosProton6Animacao.dat phaseSpaceP6_2.dat
