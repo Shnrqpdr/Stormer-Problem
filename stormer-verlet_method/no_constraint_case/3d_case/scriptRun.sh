22b414f8e2c29e29eed4963613347433936a5b1e
Shnrqpdr <hnrqpdr.sc@gmail.com>
2022-09-07 10:31:59 -0300

    New plots of new particles

--- stormer-verlet_method/no_constraint_case/3d_case/scriptRun.sh
+++ stormer-verlet_method/no_constraint_case/3d_case/scriptRun.sh
@@ -5,3 +5,6 @@
 ./sv3d 2 3.0 50.0 0.0 10.0 0.5 0.0 dadosProton4Animacao.dat phaseSpaceP4_2.dat
 ./sv3d 2 3.0 75.0 3.14 10.0 0.5 0.0 dadosProton5Animacao.dat phaseSpaceP5_2.dat
 ./sv3d 2 3.0 90.0 5.23 10.0 0.5 0.0 dadosProton6Animacao.dat phaseSpaceP6_2.dat
+./sv3d 2 3.0 100.0 5.23 10.0 0.5 0.0 dadosProton7Animacao.dat phaseSpaceP7_2.dat
+./sv3d 2 3.0 120.0 5.23 10.0 0.5 0.0 dadosProton8Animacao.dat phaseSpaceP8_2.dat
+./sv3d 2 3.0 140.0 5.23 10.0 0.5 0.0 dadosProton9Animacao.dat phaseSpaceP9_2.dat
