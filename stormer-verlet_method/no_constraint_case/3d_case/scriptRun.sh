f65ee9e520da32e999e6b82a4e0112ae1b6d5824
Shnrqpdr <hnrqpdr.sc@gmail.com>
2023-01-04 19:08:58 -0300

    Novas figuras

--- stormer-verlet_method/no_constraint_case/3d_case/scriptRun.sh
+++ stormer-verlet_method/no_constraint_case/3d_case/scriptRun.sh
@@ -8,3 +8,9 @@
 ./sv3d 2 3.0 100.0 5.23 10.0 0.5 0.0 dadosProton7Animacao.dat phaseSpaceP7_2.dat
 ./sv3d 2 3.0 120.0 5.23 10.0 0.5 0.0 dadosProton8Animacao.dat phaseSpaceP8_2.dat
 ./sv3d 2 3.0 140.0 5.23 10.0 0.5 0.0 dadosProton9Animacao.dat phaseSpaceP9_2.dat
+./sv3d 2 3.0 100.0 5.23 10.0 0.5 0.0 dadosProton10Animacao.dat phaseSpaceP10_2.dat
+./sv3d 2 3.0 100.0 3.23 5.0 1.0 0.05 dadosProton11Animacao.dat phaseSpaceP11_2.dat
+./sv3d 2 3.0 100.0 3.23 7.5 0.5 0.1 dadosProton12Animacao.dat phaseSpaceP12_2.dat
+./sv3d 2 3.0 100.0 3.23 10.0 0.25 0.15 dadosProton13Animacao.dat phaseSpaceP13_2.dat
+./sv3d 2 3.0 100.0 3.23 12.5 0.125 0.2 dadosProton14Animacao.dat phaseSpaceP14_2.dat
+./sv3d 2 3.0 100.0 3.23 15.0 0.075 0.25 dadosProton15Animacao.dat phaseSpaceP15_2.dat
